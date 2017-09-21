77c143dd5344c8ab1244e090e698149678dcc793
Esteban Miño <efmino@uc.cl>
2017-09-21 18:09:27 -0300

    take and save locally camera pictures #110;0.7

--- app/src/main/java/com/construapp/construapp/LessonActivity.java
+++ app/src/main/java/com/construapp/construapp/LessonActivity.java
@@ -1,16 +1,31 @@
 package com.construapp.construapp;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.construapp.construapp.models.Lesson;
+
+import java.io.File;
 
 public class LessonActivity extends AppCompatActivity {
 
@@ -20,6 +35,13 @@ public class LessonActivity extends AppCompatActivity {
     private TextView  lessonDescription;
 
     private Lesson lesson = new Lesson();
+    private FloatingActionButton fabCamera;
+
+    private static final int WRITE_EXTERNAL_REEQUEST = 1886;
+    private static final int CAMERA_REQUEST = 1888;
+    private static  final int CAMERA_REQUEST_PICTURE = 1887;
+
+    private String mPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,14 +57,114 @@ public class LessonActivity extends AppCompatActivity {
         lessonName.setText(lesson.getName());
         lessonDescription.setText(lesson.getDescription());
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        fabCamera = (FloatingActionButton) findViewById(R.id.fab_camera);
+
+        setFabCameraOnClickListener();
+
+    }
+
+    public void setFabCameraOnClickListener() {
+        fabCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                if (ContextCompat.checkSelfPermission(LessonActivity.this,
+                        Manifest.permission.CAMERA)
+                        != PackageManager.PERMISSION_GRANTED) {
+
+                    // Should we show an explanation?
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(LessonActivity.this,
+                            Manifest.permission.CAMERA)) {
+                        // Show an explanation to the user *asynchronously* -- don't block
+                        // this thread waiting for the user's response! After the user
+                        // sees the explanation, try again to request the permission.
+                        Snackbar.make(view, "Debes autorizar a la aplicación para tomar fotos", Snackbar.LENGTH_LONG)
+                                .setAction("Action", null).show();
+
+
+                    } else {
+                        // No explanation needed, we can request the permission.
+                        ActivityCompat.requestPermissions(LessonActivity.this,
+                                new String[]{Manifest.permission.CAMERA},
+                                CAMERA_REQUEST);
+                        // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                        // app-defined int constant. The callback method gets the
+                        // result of the request.
+                    }
+                }
+                else {
+                    dispatchTakePictureIntent();
+                    //intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                    //startActivityForResult(intent, CAMERA_REQUEST_PICTURE);
+                }
             }
         });
+    }
+
+    private void dispatchTakePictureIntent() {
+        String APP_DIRECTORY = "MyConceptsApp";
+
+        File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM), APP_DIRECTORY);
+        boolean isDirectoryCreated = file.exists();
+
+        if (!isDirectoryCreated) {
+            isDirectoryCreated = file.mkdir();
+        }
+
+        Long timestamp = System.currentTimeMillis() / 1000;
+        String imageName = timestamp.toString() + ".jpg";
+
+        mPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM) +
+                File.separator +
+                APP_DIRECTORY + File.separator + imageName;
+        File newFile = new File(file, imageName);
+
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        takePictureIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(newFile));
+        startActivityForResult(takePictureIntent, CAMERA_REQUEST_PICTURE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case CAMERA_REQUEST: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                    //startActivityForResult(intent, CAMERA_REQUEST_PICTURE);
+                    dispatchTakePictureIntent();
+
+                } else {
+                    Toast.makeText(this, "Hasta que no entregues acceso a la cámara, " +
+                            "no la podemos mostrar", Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK) {
+            switch (requestCode){
+                case CAMERA_REQUEST_PICTURE:
+
+                    MediaScannerConnection.scanFile(this,
+                            new String[]{mPath}, null,
+                            new MediaScannerConnection.OnScanCompletedListener() {
+                                @Override
+                                public void onScanCompleted(String path, Uri uri) {
+                                    Log.i("ExternalStorage", "scanned"+path+":");
+                                    Log.i("ExternalStorage", "-> Uri"+uri);
+                                }
+                            });
+                    //Bitmap bitmap = BitmapFactory.decodeFile(mPath);
+                    //imageView.setImageBitmap(bitmap);
+                    //setFileToUpload();
+            }
+        }
     }
 
     public void setLesson() {
