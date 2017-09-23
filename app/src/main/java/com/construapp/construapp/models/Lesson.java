578342d3e5c428a23a440a46cee6f8f66f13ebde
Esteban Miño <efmino@uc.cl>
2017-09-23 02:52:21 -0300

    multimedia class #110;0.5

--- app/src/main/java/com/construapp/construapp/models/Lesson.java
+++ app/src/main/java/com/construapp/construapp/models/Lesson.java
@@ -1,13 +1,29 @@
 package com.construapp.construapp.models;
 
+import java.util.ArrayList;
+
 /**
  * Created by ESTEBANFML on 18-09-2017.
  */
 
 public class Lesson {
 
+    private ArrayList<MultimediaFile> multimediaFiles;
     private String name;
     private String description;
+
+
+    public ArrayList<MultimediaFile> getMultimediaFiles() {
+        return multimediaFiles;
+    }
+
+    public void setMultimediaFiles(ArrayList<MultimediaFile> multimediaFiles) {
+        this.multimediaFiles = multimediaFiles;
+    }
+
+    public void initMultimediaFiles() {
+        this.multimediaFiles = new ArrayList<>();
+    }
 
     public void setName(String name) {
         this.name = name;
@@ -23,6 +39,12 @@ public class Lesson {
 
     public String getDescription() {
         return description;
+    }
+
+    public void addMultimediaFile(String path) {
+        MultimediaFile multimediaFile  = new MultimediaFile();
+        multimediaFile.setmPath(path);
+        multimediaFiles.add(multimediaFile);
     }
 
 }
---
+++ app/src/main/java/com/construapp/construapp/models/MultimediaFile.java
@@ -0,0 +1,19 @@
+package com.construapp.construapp.models;
+
+/**
+ * Created by ESTEBANFML on 23-09-2017.
+ */
+
+public class MultimediaFile {
+
+    private String mPath;
+
+    public String getmPath() {
+        return mPath;
+    }
+
+    public void setmPath(String mPath) {
+        this.mPath = mPath;
+    }
+
+}
