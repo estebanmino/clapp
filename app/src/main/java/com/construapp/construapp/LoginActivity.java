45368f416fc6e7914a27e8f11f37134eb072ecea
Jose Pablo Gonzalez <jpgonza5@uc.cl>
2017-09-23 17:08:59 -0300

    Api connection TODO: fix InputStream problem #105;6

--- app/src/main/java/com/construapp/construapp/LoginActivity.java
+++ app/src/main/java/com/construapp/construapp/LoginActivity.java
@@ -8,12 +8,16 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.construapp.construapp.models.RetrieveFeedTask;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText editEmail;
     private EditText editPassword;
     private Button btnSignin;
+    RetrieveFeedTask myAsyncTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +27,15 @@ public class LoginActivity extends AppCompatActivity {
         editEmail = (EditText) findViewById(R.id.edit_email);
         editPassword = (EditText) findViewById(R.id.edit_password);
         btnSignin = (Button) findViewById(R.id.btn_sigin);
+        myAsyncTask = new RetrieveFeedTask();
 
         btnSignin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(MainActivity.getIntent(LoginActivity.this));
+                //Toast.makeText(LoginActivity.this,editEmail.getText().toString(),Toast.LENGTH_SHORT).show();
+                myAsyncTask.execute(editEmail.getText().toString(),editPassword.getText().toString());
+                Toast.makeText(LoginActivity.this,myAsyncTask.out,Toast.LENGTH_SHORT).show();
+                //startActivity(MainActivity.getIntent(LoginActivity.this));
             }
         });
 
---
+++ app/src/main/java/com/construapp/construapp/models/RetrieveFeedTask.java
@@ -0,0 +1,145 @@
+package com.construapp.construapp.models;
+
+import android.os.AsyncTask;
+import android.widget.EditText;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.net.HttpURLConnection;
+import org.json.JSONObject;
+import org.json.JSONArray;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.BufferedWriter;
+import android.util.Log;
+
+/**
+ * Created by jose on 22-09-17.
+ */
+
+public class RetrieveFeedTask extends AsyncTask<String, Integer, String> {
+
+    private Exception exception;
+    public String out;
+
+    protected void onPreExecute() {
+        //progressBar.setVisibility(View.VISIBLE);
+        //responseView.setText("");
+    }
+
+    protected String doInBackground(String... str) {
+
+        boolean result = false;
+        String email = str[0];
+        String pass = str[1];
+
+        JSONObject object = new JSONObject();
+        JSONArray Session = new JSONArray();
+
+
+
+        try {
+
+            object.put("email", email);
+            object.put("password", pass);
+
+        } catch (Exception ex) {
+
+        }
+        Session.put(object);
+        JSONObject obj= new JSONObject();
+
+        try {
+
+            obj.put("Session",Session.toString());
+
+        } catch (Exception ex) {
+
+        }
+
+
+
+        try {
+            URL url = new URL("http://construapp-api.ing.puc.cl/sessions/");
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setDoOutput(true);
+            urlConnection.setDoInput(true);
+            urlConnection.setRequestMethod("POST");
+            urlConnection.setRequestProperty("Content-Type", "application/json");
+            String input = obj.toString();
+            urlConnection.connect();
+            urlConnection.getOutputStream().write(input.getBytes("UTF-8"));
+
+
+            OutputStream os = urlConnection.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(
+                    new OutputStreamWriter(os, "UTF-8"));
+            writer.write(input);
+            writer.flush();
+            writer.close();
+            os.close();
+
+
+            //InputStream response=urlConnection.getInputStream();
+            InputStream _is;
+            if (urlConnection.getResponseCode() / 100 == 2) { // 2xx code means success
+                _is = urlConnection.getInputStream();
+            } else {
+
+                _is = urlConnection.getErrorStream();
+
+                String result2 = _is.toString();
+                Log.i("Error != 2xx", result2);
+            }
+
+            //OutputStream os = urlConnection.getOutputStream();
+            //os.write(input.getBytes());
+            //os.flush();
+
+
+
+
+            //BufferedReader br = new BufferedReader(new InputStreamReader(
+              //      (response)));
+
+            String output;
+            //System.out.println("Output from Server .... \n");
+            //while ((output = br.readLine()) != null) {
+                //System.out.println(output);
+            //}
+
+            urlConnection.disconnect();
+
+            if (urlConnection.getResponseCode() == 200) {
+                // Success
+                // Further processing here
+            }
+
+            else {
+                // Error handling code goes here
+            }
+
+            //out= output;
+            return null;
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+            return null;
+
+        }
+
+
+    }
+
+    protected void onPostExecute(String response) {
+        if(response == null) {
+            response = "THERE WAS AN ERROR";
+        }
+        //progressBar.setVisibility(View.GONE);
+        //Log.i("INFO", response);
+        //responseView.setText(response);
+    }
+}
