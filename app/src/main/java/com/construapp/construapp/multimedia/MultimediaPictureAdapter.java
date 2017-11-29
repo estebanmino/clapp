b97d6eddeb78d4192031006dccf2451fab010ee5
Esteban Miño <efmino@uc.cl>
2017-11-29 01:10:16 -0300

    pictures finished

--- app/src/main/java/com/construapp/construapp/multimedia/MultimediaPictureAdapter.java
+++ app/src/main/java/com/construapp/construapp/multimedia/MultimediaPictureAdapter.java
@@ -69,7 +69,7 @@ public class MultimediaPictureAdapter extends MultimediaAdapter {
             Bitmap bitmap =  (Bitmap)LRUCache.getInstance().getLru().get(multimediaFile.getApiFileKey());
             holder.imageThumbnail.setImageBitmap(ThumbnailUtils.extractThumbnail(bitmap, 80, 80));
         }
-        else if (multimediaFile.getmPath() != null && new File(multimediaFile.getmPath()).exists()) {
+        else if (multimediaFile.getmPath() != null && !multimediaFile.getmPath().contains("cache") && new File(multimediaFile.getmPath()).exists()) {
             Bitmap bitmap = BitmapFactory.decodeFile(multimediaFile.getmPath());
             Bitmap bmRotated = rotateBitmap(bitmap, multimediaFile);
             holder.imageThumbnail.setImageBitmap(ThumbnailUtils.extractThumbnail(bmRotated, 80, 80));
@@ -116,16 +116,13 @@ public class MultimediaPictureAdapter extends MultimediaAdapter {
                         intent.setAction(Intent.ACTION_VIEW);
 
                         if (multimediaFile.getApiFileKey() != null && LRUCache.getInstance().getLru().get(multimediaFile.getApiFileKey()) != null) {
-                            Log.i("INLRU", "true");
                             String path = MediaStore.Images.Media.insertImage(getContext().getContentResolver(),
                                     (Bitmap) LRUCache.getInstance().getLru().get(multimediaFile.getApiFileKey()), "Title", null);
                             intent.setDataAndType(Uri.parse(path), FILE_TYPE);
                         } else {
-                            Log.i("INCACHE", "true");
-                            File file = new File(getContext().getCacheDir(), multimediaFile.getFileName());
-                            Log.i("INCACHE", multimediaFile.getmPath());
-                            intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-                            intent.setDataAndType(Uri.parse(multimediaFile.getmPath()), FILE_TYPE);
+                            intent.setDataAndType(
+                                    Uri.fromFile(new File(multimediaFile.getmPath()))
+                            , FILE_TYPE);
                         }
                         view.getContext().startActivity(intent);
                     }
